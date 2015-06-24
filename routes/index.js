e74830653b12a719f14cf324292dad7376b856ae
jmblanco <jmblanco1986@gmail.com>
2015-06-24 14:56:16 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,20 @@ var quizController = require('../controllers/quiz_controller');
 
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
-/* GET author page */
+
+// GET author page
 router.get('/author', function(req, res, next) {
   res.render('author', {});
 });
 
-/* GET quizes pages */
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+// GET quizes pages
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
